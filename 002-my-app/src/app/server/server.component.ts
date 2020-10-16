d54867ba4502bd83e5673c468f7fc96fefeabc4b
Frighi <71279783+Frighi@users.noreply.github.com>
2020-10-16 13:08:13 +0200

    Utilizzo del Servers

--- 002-my-app/src/app/server/server.component.ts
+++ 002-my-app/src/app/server/server.component.ts
@@ -5,5 +5,17 @@ import { Component } from '@angular/core';
     templateUrl: './server.component.html'
 })
 export class ServerComponent{
-    
+    serverId:number;
+    serverStatus: string;
+    constructor(){
+        this.serverId=this.gettRandomInt(1,100)
+    }
+    getServerStatus(){
+        this.serverStatus=this.gettRandomInt(0,1)==0 ? 'offline' : 'online';
+        return this.serverStatus;
+    }
+    gettRandomInt(min,max)
+    {
+        return Math.floor(Math.random()*(max-min+1))+ min;
+    }
 }
