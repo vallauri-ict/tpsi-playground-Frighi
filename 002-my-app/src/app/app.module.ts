a338a7588ff1de89ade827d9f6191becf5913b36
Frighi <71279783+Frighi@users.noreply.github.com>
2020-10-27 09:30:58 +0100

    Esercitazione Angular

--- 002-my-app/src/app/app.module.ts
+++ 002-my-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import {FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { PracticingDatabindingComponent } from './practicing-databinding/practicing-databinding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    PracticingDatabindingComponent
   ],
   imports: [
     BrowserModule,
